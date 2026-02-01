662152ddc44a3622c4185824cc0fff0330f29517
Efrain Ortiz <efrainsis74@gmail.com>
2026-02-01 00:17:35 -0300

    chore: export main blueprint from routes package

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
+from app.routes.main_routes import main_bp
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def home():
-        return "Flask OK 🚀"
+    # Registrar blueprints
+    app.register_blueprint(main_bp)
 
     return app
