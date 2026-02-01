d798cee16d96b74a3ef70c7adb4c38365888c6de
Efrain Ortiz <efrainsis74@gmail.com>
2026-02-01 00:24:52 -0300

    feat: add health check endpoint

--- backend/app/routes/main_routes.py
+++ backend/app/routes/main_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, jsonify
 
+# Blueprint = "mini-app" de Flask para agrupar rutas relacionadas.
+# Lo registramos luego en create_app() usando app.register_blueprint(...)
 main_bp = Blueprint("main", __name__)
 
 @main_bp.route("/api/health")
+#  Endpoint de salud (health check).
 def health():
     return jsonify({"status": "ok"})
