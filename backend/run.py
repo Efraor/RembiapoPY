2f748707f8cb95b6eef31b02dcb8eeb0607fddb7
Efrain Ortiz <efrainsis74@gmail.com>
2026-02-02 16:29:37 -0300

    fix(backend): fix app startup and dependencies

--- backend/run.py
+++ backend/run.py
@@ -1,15 +1,12 @@
 from app import create_app
 
-<<<<<<< HEAD
 
 # Creamos la instancia de Flask ya configurada (con blueprints registrados, etc.)
 app = create_app()
 
 if __name__ == "__main__":
     app.run()
-=======
 app = create_app()
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
->>>>>>> diego/main
